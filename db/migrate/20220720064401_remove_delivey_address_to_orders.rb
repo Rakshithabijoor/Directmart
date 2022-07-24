c44faf462d9ff204ebcfa52fd62d9c35727c91cb
rbijoor <rbijoor@C02F2T1JMD6M.local>
2022-07-24 22:56:49 +0530

    removed migrations and added product images

--- db/migrate/20220720064401_remove_delivey_address_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDeliveyAddressToOrders < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :orders, :delivery_address
-  end
-end
--- db/migrate/20220720071216_remove_delivey_address_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDeliveyAddressFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :delivery_address, :string
-  end
-end
